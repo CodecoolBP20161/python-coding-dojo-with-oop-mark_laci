8efb21946bf9b1fbc0da455b6641967b2a4db0bb
Milu László <hockeylaci@gmail.com>
2016-09-20 09:45:44 +0200

    5 out of 7 run as intended

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 
+class ContactList(list):
+    @classmethod
+    def search(cls, name):
+        
+
 class Contact:
-    pass
+    all_contacts = ContactList()
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)
+
+    @classmethod
+    def reset_contacts(cls):
+        cls.all_contacts = []
+
+
+class Supplier(Contact):
+    all_orders = {}
+
+    def order(self, order):
+        if self.email not in Supplier.all_orders.keys():
+            Supplier.all_orders[self.email] = [order]
+        else:
+            Supplier.all_orders[self.email].append(order)
