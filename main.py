09d7693c0dea2a0d305d435ca6b29ce17a18155d
Milu László <hockeylaci@gmail.com>
2016-09-21 09:34:26 +0200

    all done

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 
 class ContactList(list):
-    @classmethod
-    def search(cls, name):
-        
+
+    def search(self, name):
+        return_list = []
+        for contact in self:
+            if name in contact.name:
+                return_list.append(contact)
+        return return_list
+
+    def longest_name(self):
+        if len(self) > 0:
+            longest = Contact("", "")
+            for contact in self:
+                if len(contact.name) > len(longest.name):
+                    longest = contact
+            return longest.name
+        else:
+            return None
+
 
 class Contact:
     all_contacts = ContactList()
@@ -14,7 +29,7 @@ class Contact:
 
     @classmethod
     def reset_contacts(cls):
-        cls.all_contacts = []
+        cls.all_contacts = ContactList()
 
 
 class Supplier(Contact):
